d4da61a057bb35eba3a752cfff444d5f5dd1474f
Pujap84 <81335331+Pujap84@users.noreply.github.com>
2021-09-20 20:30:26 +1000

    package.json file edited

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 process.env.NODE_ENV === "production" || require("dotenv").config();
+const path = require("path");
 const express = require("express");
 let app = express();
 const PORT = 3001;
 let getMessage = require("./chatbot.js");
 
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
-
+app.use(express.static("./client/build"));
 // middleware to read JSON body data
 app.use(express.json());
 
@@ -14,6 +15,17 @@ app.get("/api/message", async (req, res) => {
     const response = await getMessage(req.query.message);
     res.json(response);
 });
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("./client/build"));
+
+    app.get("/*", (req, res) => {
+        res.sendFile(
+            path.join(__dirname, "..", "client", "build", "index.html")
+        );
+    });
+}
+
 // app.post("/api/message", async (req, res) => {
 //     // console.log(req.body.message);
 //     const message = req.body.message;
