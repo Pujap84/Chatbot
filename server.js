93ec5816fb5c66e5d1fd5d05982a7273094874dc
Pujap84 <81335331+Pujap84@users.noreply.github.com>
2021-09-20 21:44:04 +1000

    changes to server.js

--- server.js
+++ server.js
@@ -6,22 +6,20 @@ const port =
     process.env.PORT || (process.env.NODE_ENV === "production" && 3000) || 3001;
 
 let getMessage = require("./chatbot.js");
-const sessionConfig = {
-    secret: process.env.SESSION_SECRET,
-    cookie: {},
-};
+
 app.use(express.json());
 app.listen(port, () => console.log(`Server is listening on port: ${port}`));
-app.use(express.static("./client/build"));
+
 // middleware to read JSON body data
 
-app.use(session(sessionConfig));
-
 //Routes;
+app.get("/api/message", async (req, res) => {
+    const response = await getMessage(req.query.message);
+    res.json(response);
+});
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("./client/build"));
-    sessionConfig.cookie.secure = true;
     app.set("trust proxy", 1);
 
     app.get("/*", (req, res) => {
@@ -31,10 +29,6 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-app.get("/api/message", async (req, res) => {
-    const response = await getMessage(req.query.message);
-    res.json(response);
-});
 // app.post("/api/message", async (req, res) => {
 //     // console.log(req.body.message);
 //     const message = req.body.message;
